6014f31b9405f1cc52d2611a81d0acffa0d0ee2d
JonGaray <jon.garay@ikasle.egibide.org>
2024-10-17 08:53:16 +0200

    Conexion BD

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+define("DB_HOST", "172.20.227.241");
+define("DB", "grupo4_2425");
+define("DB_USER", "grupo4_2425");
+define("DB_PASS", "UMq7.E-[5Lt_@oMA");
+
+define("DEFAULT_CONTROLLER", "Preguntas");
+define("DEFAULT_ACTION", "list");
+
+define("PAGINATION", 10);
+?>
