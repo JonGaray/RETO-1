f9f196949103b418a60198d1625443d47e895bbc
JonGaray <jon.garay@ikasle.egibide.org>
2024-10-17 09:30:33 +0200

    Modelo y Controladores inicializados

---
+++ controller/RespuestaController.php
@@ -0,0 +1,15 @@
+<?php
+require_once "model/Respuesta.php";
+
+class RespuestaController{
+    public $page_title;
+    public $view;
+    public $model;
+
+    public function __construct()
+    {
+        $this->view = "list";
+        $this->page_title = "";
+        $this->model = new Respuesta();
+    }
+}
---
+++ model/Pregunta.php
@@ -0,0 +1,12 @@
+<?php
+class Pregunta{
+    private $table = "preguntas";
+    private $connection;
+    public function __construct(){
+        $this->getConnection();
+    }
+    public function getConnection(){
+        $dbObj =new Db();
+        $this->connection = $dbObj->conection_db;
+    }
+}
