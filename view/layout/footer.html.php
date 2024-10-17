e2dbcd47058c85331b79d543f32608223f3c4247
ibailopezdelapuente2 <ibai.lopezdelapuente@ikasle.egibide.org>
2024-10-17 10:53:17 +0200

    Commit control, footer a medias

---
+++ view/layout/footer.html.php
@@ -0,0 +1,20 @@
+<link rel="stylesheet" href="../../assets/styles.css">
+<footer>
+    <div class="logo-copyright">
+        <h1>AERGIBIDE</h1>
+        <p>AERGIBIDE <br/>
+        Copyright © AERGIBIDE S.A. all rights reserved</p>
+    </div>
+    <div class="contacto">
+        <h2>Contact with us</h2>
+        <form class="formulario-contacto" action="" method="post">
+            <input type="text" placeholder="Correo">
+            <textarea rows="8" name="descripcion-contacto" id="descripcion-contacto" placeholder="Descripcion"></textarea>
+        </form>
+    </div>
+    <div class="rrss">
+            <p>instagram</p>
+            <p>twitter</p>
+            <p>facebook</p>
+        </div>
+</footer>
