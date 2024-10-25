4803caa392567fd4280a1f24326d6d6f5cf713d7
JonGaray <jon.garay@ikasle.egibide.org>
2024-10-25 11:34:37 +0200

    Archivos css creados

--- view/layout/header.php
+++ view/layout/header.php
@@ -8,7 +8,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Italianno&display=swap" rel="stylesheet">
 
-    <link rel="stylesheet" href="assets/styles.css">
+    <link rel="stylesheet" href="assets/estilos/layout.css">
     <title>Document</title>
     <style>
         *{
