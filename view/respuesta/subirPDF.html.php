3b6dfc9017fc932a559e405a47a5ba392cf48ac9
JordiFernandezUralde <jordi.fernandez@ikasle.egibide.org>
2024-10-30 09:35:26 +0100

    guia casi terminada

--- view/respuesta/subirPDF.html.php
+++ view/respuesta/subirPDF.html.php
@@ -1,67 +1,6 @@
-<style>
-article{
-display: flex;
-justify-content: center;
-align-items: center;
-}
-
-.container {
-background-color: #3a4a6b;
-padding: 50px;
-border-radius: 15px;
-box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
-width: 300px;
-text-align: center;
-margin: 2em;
-}
-
-/* Estilos del título */
-.container h2 {
-background-color: #d3d3d3;
-padding: 10px;
-border-radius: 4px;
-color: #333;
-margin-bottom: 20px;
-font-size: 1.2em;
-}
-
-/* Estilos de los inputs */
-.container input[type="text"],
-.container input[type="file"] {
-width: 100%;
-padding: 10px;
-margin: 10px 0;
-border: none;
-border-radius: 4px;
-background-color: #d3d3d3;
-color: #555;
-}
-
-/* Estilos de los botones */
-.container .buttons {
-display: flex;
-justify-content: space-between;
-margin-top: 15px;
-}
-
-.container button {
-width: 48%;
-padding: 10px;
-border: none;
-border-radius: 4px;
-background-color: #d3d3d3;
-color: #333;
-cursor: pointer;
-font-size: 1em;
-}
-
-.container button:hover {
-background-color: #bcbcbc;
-}
-</style>
 
 
-<article>
+<article class="subirPDF">
     <div class="container">
         <h2>Subir guía de reparación</h2>
         <form action="index.php?controller=respuesta&action=subirPDF" method="post" enctype="multipart/form-data">
--- view/respuesta/vistaPDF.html.php
+++ view/respuesta/vistaPDF.html.php
@@ -1,42 +1,28 @@
-<style>
-    article{
-        display: flex;
-        flex-direction: column;
-    }
-
-    .table-header {
-        background-color: #2D3E50; /* Fondo azul oscuro */
-        padding: 10px;
-        display: flex;
-        align-items: center;
-        justify-content: space-around;
-        flex-direction: row;
-        color: #E0E0E0; /* Texto gris claro */
-        font-size: 16px;
-        font-weight: normal;
-        gap: 20px;
-        width: 80%;
-        margin: 20px;
-        border-radius:15px ;
-    }
-
-    /* Estilos para las columnas */
-    .table-header .column {
-        flex:1;
-        text-align: center;
-    }
-</style>
-
-<a href="index.php?controller=respuesta&action=subirPDF">effefef</a>
 
 
 
-<article>
+<article class="vistaPDF">
+    <a href="index.php?controller=respuesta&action=subirPDF" class="subirDocu">Subir guia de reparacion</a>
+    <div class="table-header">
+        <span class="column">Documento:</span>
+        <span class="column">Descargar: </span></a>
+        <span class="column">Propietario:</span>
+        <?php if (isset($_COOKIE['rol_usuario']) && $_COOKIE['rol_usuario'] == 'admin') { ?>
+        <p><?php echo "Opciones:"?></p>
+        <?php } ?>
+
+    </div>
     <?php foreach ($dataToView["data"] as $pdf): ?>
     <div class="table-header">
-        <span class="column">Nombre: <?php echo    $pdf["nombre_documento"]?></span>
+        <span class="column"><?php echo $pdf["nombre_documento"]?></span>
         <span class="column"> <a href="index.php?controller=respuesta&action=descargarPDF&id=<?php echo $pdf["id_documento"]?>"><img src="assets/Images/download.png" ></span></a>
         <span class="column"><?php echo $pdf["nombre_usuario"]?></span>
+        <?php if (isset($_COOKIE['rol_usuario']) && $_COOKIE['rol_usuario'] == 'admin') { ?>
+            <form action="index.php?controller=respuesta&action=deletePDF&id=<?php echo $pdf["id_documento"]?>" method="post">
+                <input type="hidden" name="id" value="<?php echo $pdf['id_documento']; ?>">
+                <button type="submit" class="boton-eliminarPDF">Eliminar</button>
+            </form>
+        <?php } ?>
     </div>
         <?php endforeach; ?>
 </article>
