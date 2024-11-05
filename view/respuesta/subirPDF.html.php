f1077895373d737972e6c5ee3af4bafb2444c340
JordiFernandezUralde <jordi.fernandez@ikasle.egibide.org>
2024-11-05 13:18:42 +0100

    terminado guia en usuariop

--- view/respuesta/subirPDF.html.php
+++ view/respuesta/subirPDF.html.php
@@ -1,11 +1,23 @@
 
 
 <article class="subirPDF">
-    <div class="container">
+    <div class="container1">
         <h2>Subir guía de reparación</h2>
         <form action="index.php?controller=respuesta&action=subirPDF" method="post" enctype="multipart/form-data">
             <input type="text" name="nombre" placeholder="Nombre" required>
-            <input type="file" name="archivo" accept=".pdf" required>
+            <div class="container">
+                <div class="folder">
+                    <div class="front-side">
+                        <div class="tip"></div>
+                        <div class="cover"></div>
+                    </div>
+                    <div class="back-side cover"></div>
+                </div>
+                <label class="custom-file-upload">
+                    <input class="title" type="file" name="archivo" id="archivo"/>
+                    Seleccionar archivo
+                </label>
+            </div>
             <div class="buttons">
                 <button type="submit">Aceptar</button>
                 <button type="button" onclick="window.location.href='index.php?controller=pregunta&action=list'">Cancelar</button>
