dc70ef7cd126b0639baeb0a231b708bc8c7c3628
JonGaray <jon.garay@ikasle.egibide.org>
2024-11-06 10:14:43 +0100

    Cambio de botones

--- view/respuesta/vistaPDF.html.php
+++ view/respuesta/vistaPDF.html.php
@@ -12,7 +12,11 @@
     <?php foreach ($dataToView["data"] as $pdf): ?>
     <div class="table-header">
         <span class="column"><?php echo $pdf["nombre_documento"]?></span>
-        <span class="column"> <a href="index.php?controller=respuesta&action=descargarPDF&id=<?php echo $pdf["id_documento"]?>"><img src="assets/Images/Iconos/download.png" ></span></a>
+        <form method="post" action="index.php?controller=respuesta&action=descargarPDF&id=<?php echo $pdf["id_documento"]?>">
+            <button class="button">
+                <span class="button-content">Descargar</span>
+            </button>
+        </form>
         <span class="column"><?php echo $pdf["nombre_usuario"]?></span>
         <?php if (isset($_COOKIE['rol_usuario']) && $_COOKIE['rol_usuario'] == 'admin') { ?>
             <form action="index.php?controller=respuesta&action=deletePDF&id=<?php echo $pdf["id_documento"]?>" method="post">
