ea450e6442afd92e040a511b69ad1fafc641687d
JonGaray <jon.garay@ikasle.egibide.org>
2024-11-08 12:06:18 +0100

    Respuesta.css breakpoints 66%

--- view/respuesta/vistaPDF.html.php
+++ view/respuesta/vistaPDF.html.php
@@ -4,22 +4,26 @@
         <span class="column">Documento:</span>
         <span class="column">Descargar: </span></a>
         <?php if (isset($_COOKIE['rol_usuario']) && $_COOKIE['rol_usuario'] == 'admin') { ?>
-        <p><?php echo "Opciones:"?></p>
+        <span class="column"><?php echo "Opciones:"?></span>
         <?php } ?>
     </div>
     <?php foreach ($dataToView["data"] as $pdf): ?>
     <div class="table-header">
         <span class="column"><?php echo $pdf["nombre_documento"]?></span>
-        <form method="post" action="index.php?controller=respuesta&action=descargarPDF&id=<?php echo $pdf["id_documento"]?>">
+        <span class="column">
+            <form method="post" action="index.php?controller=respuesta&action=descargarPDF&id=<?php echo $pdf["id_documento"]?>">
             <button class="button">
                 <span class="button-content">Descargar</span>
             </button>
-        </form>
-        <?php if (isset($_COOKIE['rol_usuario']) && $_COOKIE['rol_usuario'] == 'admin') { ?>
-            <form action="index.php?controller=respuesta&action=deletePDF&id=<?php echo $pdf["id_documento"]?>" method="post">
+            </form>
+        </span>
+        <span class="column">
+            <?php if (isset($_COOKIE['rol_usuario']) && $_COOKIE['rol_usuario'] == 'admin') { ?>
+                <form action="index.php?controller=respuesta&action=deletePDF&id=<?php echo $pdf["id_documento"]?>" method="post">
                 <button type="submit" class="boton-eliminarPDF">Eliminar</button>
             </form>
-        <?php } ?>
+            <?php } ?>
+        </span>
     </div>
         <?php endforeach; ?>
 </article>
